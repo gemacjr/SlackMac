1ff62d78606020419b4f04caa1fb56cbc73dc902
Ed <edmccormic@gmail.com>
2017-08-13 16:03:13 -0600

    Initial Commit

---
+++ SlackMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SlackMac
+//
+//  Created by Ed McCormic on 8/13/17.
+//  Copyright © 2017 Swiftbeard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
